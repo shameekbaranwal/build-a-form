4fb3b363a91b9ceba81b60d5d11412f982bb79fb
Shameek Kumar Baranwal <shameekbaranwal@gmail.com>
2022-07-14 05:11:29 +0530

    Fixed bug in rendering successful submission modal.

--- src/components/PreviewForm/SubmitSuccessModal.jsx
+++ src/components/PreviewForm/SubmitSuccessModal.jsx
@@ -15,19 +15,21 @@ export default function SubmitSuccessModal({
 				</h3>
 				<p className='m-4'>your submission was made successfully.</p>
 				<ul className='mx-4'>
-					{content.map((q, index) => (
-						<li
-							key={index}
-							className='flex flex-col font-extralight'
-						>
-							<p className='font-bold'>{`Q) ${q.question}`}</p>
-							<p className=''>{`A) ${
-								q.answer.length === 0
-									? 'not answered'
-									: q.answer.join(', ')
-							}`}</p>
-						</li>
-					))}
+					{content.map((q, index) => {
+						let a = q.answer;
+						if (a.length === 0) a = 'not answered';
+						if (a instanceof Array) a = a.join(', ');
+
+						return (
+							<li
+								key={index}
+								className='flex flex-col font-extralight'
+							>
+								<p className='font-bold'>{`Q) ${q.question}`}</p>
+								<p className=''>{`A) ${a}`}</p>
+							</li>
+						);
+					})}
 				</ul>
 			</div>
 		</Modal>
